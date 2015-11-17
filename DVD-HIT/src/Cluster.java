02970b73aab2f0272fedde0da19dd8684006f041
PhaniGaddipati <phanigaddipati@gmail.com>
2015-11-17 10:42:21 -0500

    getLongestSequence method, and cluster extends HashSet instead of ArrayList (a cluster is unordered)

--- DVD-HIT/src/Cluster.java
+++ DVD-HIT/src/Cluster.java
@@ -1,8 +1,20 @@
-import java.util.ArrayList;
+import java.util.HashSet;
 
 /**
  * Created by Phani on 11/17/2015.
  */
-public class Cluster extends ArrayList<String> {
+public class Cluster extends HashSet<String> {
+
+    public String getLongestSequence() {
+        String longest = null;
+        int longestLen = Integer.MIN_VALUE;
+        for (String s : this) {
+            if (s.length() > longestLen) {
+                longestLen = s.length();
+                longest = s;
+            }
+        }
+        return longest;
+    }
 
 }

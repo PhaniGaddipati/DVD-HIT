0a3e4684555a57388406102a2cfbc64f52107be6
PhaniGaddipati <phanigaddipati@gmail.com>
2015-11-17 10:39:42 -0500

    clustering algorithm, and similarityfilter interface

--- DVD-HIT/src/DVD_HIT.java
+++ DVD-HIT/src/DVD_HIT.java
@@ -1,8 +1,6 @@
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by Phani on 11/17/2015.
@@ -15,9 +13,36 @@ public class DVD_HIT {
         this.file = inFile;
     }
 
-    public List<Cluster> cluster() throws IOException {
+    public List<Cluster> cluster(SequenceSimilarityFilter filter) throws IOException {
         List<Cluster> clusters = new ArrayList<Cluster>();
+        List<String> sequences = readSequences(file);
 
+        //Step 1, Sort by decreasing length
+        Collections.sort(sequences, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return Integer.compare(o1.length(), o2.length());
+            }
+        });
+
+        //At this points, sequences is a list of to-be-clustered seqs
+        while (sequences.size() > 0) {
+            //Start cluster
+            Cluster cluster = new Cluster();
+            cluster.add(sequences.remove(0));
+
+            // Add any sequences that are similar to the cluster
+            for (Iterator<String> it = sequences.iterator(); it.hasNext(); ) {
+                String seq = it.next();
+                if (filter.isSimilar(cluster, seq)) {
+                    cluster.add(seq);
+                    it.remove();
+                }
+            }
+
+            clusters.add(cluster);
+        }
+        //Now
         return clusters;
     }
 
---
+++ DVD-HIT/src/SequenceSimilarityFilter.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Phani on 11/17/2015.
+ */
+public interface SequenceSimilarityFilter {
+
+    /**
+     * Returns if sequence is similar to ALL sequences in cluster
+     *
+     * @param cluster  The cluster to compare against
+     * @param sequence The candidate sequence to add to cluster
+     * @return Whether sequence is similar to EVERY seq in cluster
+     */
+    public boolean isSimilar(Cluster cluster, String sequence);
+
+}

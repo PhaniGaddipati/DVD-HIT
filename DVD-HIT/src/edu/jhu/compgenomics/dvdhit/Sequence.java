ff5fcd707e8c0e9c20e3f9264c07cac786b8f549
PhaniGaddipati <phanigaddipati@gmail.com>
2015-12-07 12:08:51 -0500

    parse genus/species

--- DVD-HIT/src/edu/jhu/compgenomics/dvdhit/Sequence.java
+++ DVD-HIT/src/edu/jhu/compgenomics/dvdhit/Sequence.java
@@ -8,11 +8,17 @@ public class Sequence {
     private final String sequence;
     private final String name;
     private final String description;
+    private final String genus;
+    private final String species;
 
     public Sequence(String name, String description, String sequence) {
         this.sequence = sequence;
         this.name = name;
         this.description = description;
+        String tmp = description.toLowerCase().replace("'", "").replace("[", "").replace("]", "").trim();
+        genus = tmp.substring(0, tmp.indexOf(" "));
+        tmp = tmp.substring(tmp.indexOf(" ") + 1);
+        species = tmp.substring(0, tmp.indexOf(" "));
     }
 
     public String getSequence() {

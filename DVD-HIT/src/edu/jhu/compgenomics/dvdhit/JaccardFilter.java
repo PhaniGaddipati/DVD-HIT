a029c5439aeebab0d74f9aa7c797fd5bac4eb887
abalabazn <Andy@Andrews-MacBook-Pro-2.local>
2015-12-08 15:08:44 -0500

    jaccard filter added

---
+++ DVD-HIT/src/edu/jhu/compgenomics/dvdhit/JaccardFilter.java
@@ -0,0 +1,69 @@
+package edu.jhu.compgenomics.dvdhit;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by Phani on 11/17/2015.
+ */
+public class JaccardFilter implements SequenceSimilarityFilter {
+
+    private static final int MIN_K = 8;
+    private static final int MAX_K = 8;
+    private static final double THRESHOLD = .7;
+    // A mapping of a sequence to a map, which maps k to a set of the kmers
+    private Map<String, Map<Integer, Map<String, Integer>>> masterIndex = new HashMap<>();
+
+    @Override
+    
+    public boolean isSimilar(Cluster cluster, Sequence sequence) {
+        Sequence rep = cluster.getLongestSequence();
+        if (!masterIndex.containsKey(rep.getSequence())) {
+            addToMasterIndex(rep.getSequence());
+        }
+        Map<Integer, Map<String, Integer>> seqKmerIndexes = getStringKmerCounts(sequence.getSequence());
+        for (int k = MIN_K; k <= MAX_K; k++) {
+            Map<String, Integer> repIndex = masterIndex.get(rep.getSequence()).get(k);
+            Map<String, Integer> seqIndex = seqKmerIndexes.get(k);
+            
+            Set<String> union = new HashSet<String>(repIndex.keySet());
+            union.addAll(seqIndex.keySet());
+
+            Set<String> intersection = new HashSet<String>(repIndex.keySet());
+            intersection.retainAll(seqIndex.keySet());
+            
+            float jaccard = (float) intersection.size() / (float) union.size();
+//            System.out.println(jaccard);
+            if (jaccard < THRESHOLD) return false;
+        }
+        return true;
+    }
+
+    private void addToMasterIndex(String rep) {
+        Map<Integer, Map<String, Integer>> kmerIndex = getStringKmerCounts(rep);
+        masterIndex.put(rep, kmerIndex);
+    }
+
+    private Map<Integer, Map<String, Integer>> getStringKmerCounts(String rep) {
+        Map<Integer, Map<String, Integer>> kmerIndex = new HashMap<>();
+        String kmer;
+        for (int k = MIN_K; k <= MAX_K; k++) {
+            Map<String, Integer> kmers = new HashMap<>(3000);
+            for (int i = 0; i < rep.length() - k; i++) {
+                kmer = rep.substring(i, i + k);
+                int oldVal = kmers.get(kmer) == null ? 0 : kmers.get(kmer);
+                kmers.put(kmer, oldVal + 1);
+            }
+            kmerIndex.put(k, kmers);
+        }
+        return kmerIndex;
+    }
+
+    @Override
+    public String getName() {
+        return "Jaccard";
+    }
+}
